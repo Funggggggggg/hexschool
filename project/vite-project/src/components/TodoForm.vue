950894c1a2439d77d350b88d8af70c256d8a970c
fung <fung.8021@gmail.com>
2025-09-04 23:30:46 +0800

    整理檔案及拆分元件

---
+++ project/vite-project/src/components/TodoForm.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="inputBox">
+    <input type="text" placeholder="請輸入待辦事項" />
+    <a href="#">
+      <i class="fa fa-plus"></i>
+    </a>
+  </div>
+</template>
+<script setup></script>
---
+++ project/vite-project/src/components/TodoList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="todoList_list">
+    <ul class="todoList_tab">
+      <li><a href="#" class="active">全部</a></li>
+      <li><a href="#">待完成</a></li>
+      <li><a href="#">已完成</a></li>
+    </ul>
+    <div class="todoList_items">
+      <ul class="todoList_item">
+        <TodoItem></TodoItem>
+      </ul>
+      <div class="todoList_statistics">
+        <p>5 個已完成項目</p>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+import TodoItem from './TodoItem.vue'
+</script>
